34589c70682a5ced91f840d196b73d7d8719b7ec
Osvaldo <osvaldo.guerra.c008@gmail.com>
2024-08-31 08:32:08 -0600

    dependencies

--- news-mobile-app/app/build.gradle.kts
+++ news-mobile-app/app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Retrofit
+    implementation(libs.squaredup.retrofit2)
+    implementation(libs.squaredup.retrofit2.converter)
+
+    // Coil
+    implementation(libs.io.coil)
 }
