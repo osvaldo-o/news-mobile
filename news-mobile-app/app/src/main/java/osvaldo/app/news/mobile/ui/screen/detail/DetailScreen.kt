c181ffe42a7cc50e259e25150500bb2e618af44b
Osvaldo <osvaldo.guerra.c008@gmail.com>
2024-09-01 17:45:38 -0600

    Read full news

--- news-mobile-app/app/src/main/java/osvaldo/app/news/mobile/ui/screen/detail/DetailScreen.kt
+++ news-mobile-app/app/src/main/java/osvaldo/app/news/mobile/ui/screen/detail/DetailScreen.kt
@@ -1,13 +1,24 @@
 package osvaldo.app.news.mobile.ui.screen.detail
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -15,13 +26,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import osvaldo.app.news.mobile.R
 import osvaldo.app.news.mobile.domain.model.News
 import osvaldo.app.news.mobile.ui.viewmodel.NewsEvent
 
@@ -46,7 +62,8 @@ fun DetailScreen(
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .padding(horizontal = 14.dp),
+                .padding(horizontal = 14.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(14.dp)
         ) {
             Text(
@@ -77,6 +94,36 @@ fun DetailScreen(
                 style = MaterialTheme.typography.bodyMedium,
                 fontWeight = FontWeight.Light
             )
+            ReadFullNews(name = news.nameSource, url = news.url)
         }
     }
+}
+
+@Composable
+fun ReadFullNews(
+    name: String,
+    url: String
+) {
+    val context = LocalContext.current
+    val intent = remember {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 18.dp)
+            .clickable {
+                context.startActivity(intent)
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(id = R.string.read_full_news) + " ($name)",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = null)
+    }
+    HorizontalDivider()
 }
