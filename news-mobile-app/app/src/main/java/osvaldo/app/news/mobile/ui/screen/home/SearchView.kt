03ab359acf93d9033e9826c5d6f61cdebba4a824
Osvaldo <osvaldo.guerra.c008@gmail.com>
2024-09-01 11:05:36 -0600

    change and button clear search

--- news-mobile-app/app/src/main/java/osvaldo/app/news/mobile/ui/screen/home/SearchView.kt
+++ news-mobile-app/app/src/main/java/osvaldo/app/news/mobile/ui/screen/home/SearchView.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +16,8 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,19 +26,25 @@ fun SearchView(
     onValueChange: (String) -> Unit,
     search: () -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
     TextField(
         value = searchValue,
         onValueChange = onValueChange,
         maxLines = 1,
         singleLine = true,
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        },
         trailingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                modifier = Modifier.clickable {
-                    search()
-                }
-            )
+            if (searchValue.isNotEmpty()) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        onValueChange("")
+                    }
+                )
+            }
         },
         shape = RoundedCornerShape(16.dp),
         colors = TextFieldDefaults.colors(
@@ -45,6 +56,14 @@ fun SearchView(
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 16.dp),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                focusManager.clearFocus()
+                search()
+            }
+        ),
+        textStyle = MaterialTheme.typography.bodyLarge
     )
 }
